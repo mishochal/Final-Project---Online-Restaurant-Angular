ebdc1642cdea205ccb4d4c58d7bb38c3a186cec2
Misho Chaladze <mishochaladze4@gmail.com>
2025-01-06 19:42:57 +0400

    redeploy

--- docs/server/main.server.mjs
+++ docs/server/main.server.mjs
@@ -1,2 +1,2 @@
 import './polyfills.server.mjs';
-import{a}from"./chunk-BISJQ6Q3.mjs";import"./chunk-SWSGTMX3.mjs";import"./chunk-VVCT4QZE.mjs";export{a as default};
+import{a}from"./chunk-5UPYXOUO.mjs";import"./chunk-FTM3ODHD.mjs";import"./chunk-VVCT4QZE.mjs";export{a as default};
